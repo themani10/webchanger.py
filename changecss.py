e83982ffea282759f13c0e7a035a7327237b4775
themani10 <38328818+themani10@users.noreply.github.com>
2023-07-20 11:22:26 -0500

    Create changecss.py

---
+++ changecss.py
@@ -0,0 +1,20 @@
+#convert all px values to rem or other values
+
+import re
+
+def test(a):
+    b=float(a[1])*0.062 #multiplies 0.062 by value in group 1
+    return str(b)+'rem'
+
+
+myRe=re.compile('(\d{0,4})(px)')
+sub=myRe.sub(test, css)
+print(sub)
+
+    
+
+
+    
+    
+    
+    
