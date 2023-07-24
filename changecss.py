4af50a22cb2a6da2d5b96c7ae413b4e56bfa4f1f
themani10 <38328818+themani10@users.noreply.github.com>
2023-07-24 16:47:22 -0500

    Update changecss.py

--- changecss.py
+++ changecss.py
@@ -11,6 +11,28 @@ myRe=re.compile(r'(\d{0,4})(px)')
 sub=myRe.sub(test, css)
 print(sub)
 
+#introduce 'logical properties'
+def test(a):
+    if a.group(0) == '-left':
+        return '-inline-start'
+    if a.group(0) == '-right':
+        return '-inline-end'
+    if a.group(0) == '-top':
+        return '-block-start'
+    if a.group(0) == '-bottom':
+        return '-block-end'
+    if a.group(0) == ' top:':
+        return 'inset-block-start'
+    if a.group(0) == ' bottom:':
+        return 'inset-block-end'
+        
+     
+
+
+myRe=re.compile(r'-left|-right|-bottom|-top|\stop:|\sbottom:')
+sub=myRe.sub(test, css)
+#sub = myRe.findall(css)
+print(sub)
     
 
 
